7ef043e1af2df0aa910461067acf00d71a650784
Franco Benavides <benavidesgarciafranco@gmail.com>
2025-12-11 21:25:09 +0100

    Solución delegación eventos DOM

--- UT06.- DOM/Intemedios/Ejercicio8.js
+++ UT06.- DOM/Intemedios/Ejercicio8.js
@@ -1,8 +1,29 @@
 
 const menu = document.querySelector("#menu");
+const primerItem = menu.firstElementChild;
+const segundoItem = primerItem.nextElementSibling;
+const tercerItem = menu.lastElementChild;
 
-menu.addEventListener("click", (ev) => {
-  if (ev.target.classList.contains("perfil")) alert("Este es el perfil")
-  else if (ev.target.classList.contains("config")) alert("La configuración")
-  else if (ev.target.classList.contains("salir")) alert("Salir")
-});
+menu.addEventListener("mouseover", (ev) => {
+  if (ev.target === primerItem) console.log("Has pasado por el primer item");
+  if (ev.target === segundoItem) console.log("Has pasado por el segundo item");
+  if (ev.target === tercerItem) console.log("Has pasado por el tercer item");
+})
+
+/* menu.addEventListener("click", (ev) => {
+  const dataOp = ev.target.dataset.op;  // dataset.op es equivalente a data-op
+
+  switch(dataOp) {
+    case "perfil":
+      console.log("Has clicado en data-op perfil");
+      break;
+    case "config":
+      console.log("Has clicado en data-op config");
+      break;
+    case "salir":
+      console.log("Has clicado en data-op salir");
+      break;
+    default:
+      console.log("Click fuera de los elementos del menú");
+  }
+}); */
